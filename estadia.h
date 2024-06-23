b28709bf41adbcfd3aadce6c44d57b5578bc1f95
pedroMontandon <pedrobarbosa89@gmail.com>
2024-06-23 17:27:44 -0300

    estadia primeiraversão

---
+++ estadia.h
@@ -0,0 +1,32 @@
+#ifndef ESTADIA_H
+#define ESTADIA_H
+
+#include "pessoa.h"
+#include "estadia.h"
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+class Estadia {
+    private:
+        string dataEntrada;
+        string dataSaida;
+        int diarias;
+        int idCliente;
+        int numeroQuarto;
+    public:
+        Estadia();
+        string getDataEntrada();
+        string getDataSaida();
+        string getDiarias();
+        int getIdCliente();
+        int getNumeroQuarto();
+        void setDataEntrada(string& x);
+        void setDataSaida(string& x);
+        void setDiarias(int x);
+        void setIdClient(int x);
+        void setNumeroQuarto(int x);
+        void newEstadia();
+};   
